e86f047bbbf36f8ffc14af62d66eea71ad29624a
Jasmine Cipollone <jasmine.cipollone@aesys.tech>
2022-07-06 16:27:00 +0200

    carousel

--- src/components/ShowListMenu.js
+++ src/components/ShowListMenu.js
@@ -1,5 +1,5 @@
 import FoodCard from "./FoodCard";
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import Container from "@mui/material/Container";
 import Stack from "@mui/material/Stack";
@@ -7,9 +7,30 @@ import Box from "@mui/material/Box";
 import AppBar from "@mui/material/AppBar";
 import { Typography } from "@mui/material";
 import Cart from "./Cart";
+import { Button } from "@mui/material";
 
 function ShowListMenu() {
   const menuList = useSelector((state) => state.orders);
+
+  const [carouselLeft, setCarouselLeft] = useState(0);
+  const [carouselRight, setCarouselRight] = useState(4);
+  const menuVisibile = menuList.filter((food) => food.id >= carouselLeft && food.id < carouselRight);
+
+  function left() {
+    if (carouselLeft === 0) {
+      return;
+    }
+    setCarouselLeft(carouselLeft - 1);
+    setCarouselRight(carouselRight - 1);
+  }
+
+  function right() {
+    if (carouselLeft === menuList.length - 4) {
+      return;
+    }
+    setCarouselLeft(carouselLeft + 1);
+    setCarouselRight(carouselRight + 1);
+  }
   return (
     <>
       <AppBar position="static" color="error">
@@ -18,22 +39,30 @@ function ShowListMenu() {
         </Typography>
       </AppBar>
       <Box m={4}></Box>
-      <Container>
-        <Stack direction="row" sx={{ flexWrap: "wrap" }}>
-          {menuList.map((food) => {
-            return (
-              <FoodCard
-                key={food.id}
-                idArray={food.id}
-                name={food.name}
-                quantity={food.quantity}
-                image={food.image}
-                price={food.price}
-              />
-            );
-          })}
-        </Stack>
-      </Container>
+      <Stack direction="row">
+        <Button onClick={() => left()} color="error">
+          -
+        </Button>
+        <Container>
+          <Stack direction="row" spacing={2}>
+            {menuVisibile.map((food) => {
+              return (
+                <FoodCard
+                  key={food.id}
+                  idArray={food.id}
+                  name={food.name}
+                  quantity={food.quantity}
+                  image={food.image}
+                  price={food.price}
+                />
+              );
+            })}
+          </Stack>
+        </Container>
+        <Button onClick={() => right()} color="error">
+          +
+        </Button>
+      </Stack>
       <Cart />
     </>
   );
